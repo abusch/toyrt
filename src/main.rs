d12497c033641289031745c73acccf60b4de5e6a
Antoine Büsch <antoine.busch@gmail.com>
2018-04-25 19:11:06 +1000

    Display in real time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate cgmath as cg;
+extern crate minifb;
 extern crate rand;
 extern crate rayon;
 
@@ -12,6 +13,7 @@ use std::io::{BufWriter, Write};
 use std::sync::Arc;
 
 use cg::{prelude::*, vec3};
+use minifb::{Window, WindowOptions};
 use rand::Rng;
 use rayon::prelude::*;
 
@@ -23,6 +25,66 @@ type Vec3 = cg::Vector3<f32>;
 
 const NX: usize = 800;
 const NY: usize = 600;
+
+fn main() {
+    let mut window = match Window::new("toyrt", NX, NY, WindowOptions::default()) {
+        Ok(win) => win,
+        Err(err) => {
+            println!("Unable to create window {}", err);
+            return;
+        }
+    };
+
+    let mut buf = vec![PixelSample::new(); NX * NY];
+    let mut rendered_buf = vec![0u32; NX * NY];
+    let ratio = NX as f32 / NY as f32;
+    let mut total_samples = 0;
+    let ns = 5;
+
+    let ground: Arc<Material + Send + Sync> = Arc::new(Diffuse::new(vec3(0.8, 0.8, 0.0)));
+    let diffuse: Arc<Material + Send + Sync> = Arc::new(Diffuse::new(vec3(0.8, 0.3, 0.3)));
+    let mirror: Arc<Material + Send + Sync> = Arc::new(Mirror);
+    let world = Aggregation {
+        shapes: vec![
+            Box::new(Sphere::new(vec3(-1.0, 0.0, -1.0), 0.5, mirror.clone())),
+            Box::new(Sphere::new(vec3(0.0, 0.0, -1.0), 0.5, diffuse.clone())),
+            Box::new(Sphere::new(vec3(0.0, -100.5, -1.0), 100.0, ground.clone())),
+        ],
+    };
+    let camera_centre = vec3(0.0, 0.0, 0.5);
+    loop {
+        buf.par_chunks_mut(NX).enumerate().for_each(|(y, row)| {
+            let mut rng = rand::thread_rng();
+            for x in 0..NX {
+                for _ in 0..ns {
+                    let u = (((x as f32 + rng.next_f32()) / NX as f32) * 2.0 - 1.0) * ratio;
+                    let v = (((NY - y) as f32 + rng.next_f32()) / NY as f32) * 2.0 - 1.0;
+                    let mut ray = Ray::new(camera_centre, vec3(u, v, -1.0));
+                    row[x].add(&colour(&world, &mut ray, 0));
+                }
+            }
+        });
+        total_samples += ns;
+        print!("\rRendered {} samples...", total_samples);
+        // Update display
+        rendered_buf
+            .par_chunks_mut(NX)
+            .enumerate()
+            .for_each(|(y, row)| {
+                for x in 0..NX {
+                    let (r, g, b) = buf[x + NX * y].render();
+                    row[x] = ((r as u32) << 16) | ((g as u32) << 8) | (b as u32);
+                }
+            });
+        if window.is_open() {
+            window.update_with_buffer(&rendered_buf).unwrap();
+        } else {
+            break;
+        }
+    }
+
+    write_img(&buf);
+}
 
 pub fn colour(shape: &Shape, r: &mut Ray, depth: u32) -> Vec3 {
     if let Some(hit) = shape.intersect(r) {
@@ -46,55 +108,45 @@ pub fn colour(shape: &Shape, r: &mut Ray, depth: u32) -> Vec3 {
     }
 }
 
-fn main() {
-    let mut buf = vec![Vec3::zero(); NX * NY];
-    let ratio = NX as f32 / NY as f32;
-    let ns = 100;
-
-    let ground: Arc<Material + Send + Sync> = Arc::new(Diffuse::new(vec3(0.8, 0.8, 0.0)));
-    let diffuse: Arc<Material + Send + Sync> = Arc::new(Diffuse::new(vec3(0.8, 0.3, 0.3)));
-    let mirror: Arc<Material + Send + Sync> = Arc::new(Mirror);
-    let world = Aggregation {
-        shapes: vec![
-            Box::new(Sphere::new(vec3(-1.0, 0.0, -1.0), 0.5, mirror.clone())),
-            Box::new(Sphere::new(vec3(0.0, 0.0, -1.0), 0.5, diffuse.clone())),
-            Box::new(Sphere::new(vec3(0.0, -100.5, -1.0), 100.0, ground.clone())),
-        ],
-    };
-    let camera_centre = vec3(0.0, 0.0, 0.5);
-    buf.par_chunks_mut(NX).enumerate().for_each(|(y, row)| {
-        let mut rng = rand::thread_rng();
-        for x in 0..NX {
-            let mut col = Vec3::zero();
-            for _ in 0..ns {
-                let u = (((x as f32 + rng.next_f32()) / NX as f32) * 2.0 - 1.0) * ratio;
-                let v = (((NY - y) as f32 + rng.next_f32()) / NY as f32) * 2.0 - 1.0;
-                let mut ray = Ray::new(camera_centre, vec3(u, v, -1.0));
-                col += colour(&world, &mut ray, 0);
-            }
-
-            row[x] = col / ns as f32;
-        }
-    });
-
-    write_img(&buf);
+#[derive(Debug, Clone)]
+pub struct PixelSample {
+    sum: Vec3,
+    n_sample: u32,
 }
 
-fn write_img(img: &[Vec3]) {
+impl PixelSample {
+    pub fn new() -> PixelSample {
+        PixelSample {
+            sum: Vec3::zero(),
+            n_sample: 0,
+        }
+    }
+
+    pub fn add(&mut self, sample: &Vec3) {
+        self.sum += *sample;
+        self.n_sample += 1;
+    }
+
+    pub fn render(&self) -> (u8, u8, u8) {
+        let final_color = self.sum / self.n_sample as f32;
+        let (r, g, b) = final_color.into();
+        (
+            (f32::powf(r, 1.0 / 2.2) * 255.99) as u8,
+            (f32::powf(g, 1.0 / 2.2) * 255.99) as u8,
+            (f32::powf(b, 1.0 / 2.2) * 255.99) as u8,
+        )
+    }
+}
+
+fn write_img(img: &[PixelSample]) {
     let file = File::create("out.ppm").expect("Could not create out.ppm");
     let mut out = BufWriter::new(file);
 
     writeln!(out, "P3 {} {}\n255", NX, NY).unwrap();
     for y in 0..NY {
         for x in 0..NX {
-            let (r, g, b) = img[y * NX + x].into();
-            write!(
-                out,
-                "{} {} {} ",
-                (f32::powf(r, 1.0 / 2.2) * 255.99) as u8,
-                (f32::powf(g, 1.0 / 2.2) * 255.99) as u8,
-                (f32::powf(b, 1.0 / 2.2) * 255.99) as u8
-            ).unwrap();
+            let (r, g, b) = img[y * NX + x].render();
+            write!(out, "{} {} {} ", r, g, b).unwrap();
         }
         writeln!(out).unwrap();
     }
